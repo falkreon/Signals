004572c6a7c70f33199830c83c907d1a26a7816a
Isaac Ellingson <falkreon@gmail.com>
2012-12-30 19:59:45 -0500

    Streams (blocking pipe from an outputstream to an inputstream)
    plus read-everything-from-this-inputstream

---
+++ Signals/src/com/thoughtcomplex/io/Streams.java
@@ -0,0 +1,26 @@
+package com.thoughtcomplex.io;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Streams {
+    
+    public static void route(final InputStream in, final OutputStream out) throws IOException {
+        final int buffersize = 4096;
+        final byte[] buffer = new byte[buffersize];
+    
+        int available = 0;
+        while ((available = in.read(buffer)) >= 0)
+        {
+            out.write(buffer, 0, available);
+        }
+    }
+    
+    public static byte[] readAll(final InputStream in) throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        route(in, out);
+        return out.toByteArray();
+    }
+}
